914cd875a07866e204c683c158bca6185116aaea
Mohamed Martino <mohamedmartino@Mohameds-iMac.local>
2019-08-18 13:04:01 -0700

    SQL homework

---
+++ SQL_Submission/Queries-SQL Homework.sql
@@ -0,0 +1,75 @@
+-- Listing emplyee details
+SELECT dept_emp.emp_num, employees.last_name, employees.first_name, employees.gender, salaries.salary 
+	FROM dept_emp 
+	INNER JOIN employees ON (dept_emp.emp_num = employees.emp_num)
+	INNER JOIN salaries ON (salaries.emp_num = dept_emp.emp_num);
+
+-- Employees hired in 1986
+SELECT * FROM employees
+	WHERE EXTRACT(year FROM "hire_date") = 1986;
+	
+-- Manager information
+/*ALTER TABLE dept_manager 
+	ALTER COLUMN emp_num TYPE INT USING emp_num::integer;*/
+SELECT 
+	dept_manager.dept_num, 
+	departments.dept_name,
+	dept_manager.emp_num, 
+	employees.last_name, 
+	employees.first_name, 
+	dept_manager.from_date, 
+	dept_manager.to_date
+FROM dept_manager
+INNER JOIN departments on dept_manager.dept_num = departments.dept_num
+INNER JOIN employees on dept_manager.emp_num = employees.emp_num;
+	
+	
+-- List the deparment of each employee with the following information: 
+-- employee number, last name, first name, and department name.
+SELECT 
+	employees.emp_num,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON (employees.emp_num = dept_emp.emp_num)
+INNER JOIN departments ON (departments.dept_num = dept_emp.dept_num);
+
+
+-- Employees with the name "Hercules" and last name starts with "B"
+SELECT * from employees
+	WHERE (first_name = 'Hercules' AND last_name LIKE 'B%');
+
+
+-- Employees in the Sales department; employee number, last name, first name, and department name.
+SELECT 
+	employees.emp_num,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM employees, departments
+WHERE 
+	departments.dept_num =(SELECT dept_num FROM departments
+		WHERE dept_name = 'Sales')
+	
+	
+-- employees in the Sales and Development departments; 
+-- employee number, last name, first name, and department name.
+SELECT 
+	employees.emp_num,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM employees, departments
+WHERE 
+	departments.dept_num IN (
+		SELECT dept_num FROM departments
+		WHERE (dept_name = 'Development' OR dept_name = 'Sales')
+ORDER BY departments.dept_name DESC);
+
+
+
+
+
+
+
---
+++ SQL_Submission/Schemata-SQL Homework.sql
@@ -0,0 +1,67 @@
+﻿-- Tables' schemata
+CREATE TABLE "Departments" (
+    "dept_num" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_num"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_num" int   NOT NULL,
+    "dept_num" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_num"
+     )
+);
+
+CREATE TABLE "department_manager" (
+    "dept_num" VARCHAR   NOT NULL,
+    "emp_num" int   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_num" int   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(100)   NOT NULL,
+    "last_name" VARCHAR(100)   NOT NULL,
+    "gender" VARCHAR(5)   NOT NULL,
+    "hire_date" DATE   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_num" varchar   NOT NULL,
+    "salary" NUMERIC   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "emp_num" varchar   NOT NULL,
+    "title" VARCHAR(50)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_num" FOREIGN KEY("dept_num")
+REFERENCES "Departments" ("dept_num");
+
+ALTER TABLE "department_manager" ADD CONSTRAINT "fk_department_manager_dept_num" FOREIGN KEY("dept_num")
+REFERENCES "Departments" ("dept_num");
+
+ALTER TABLE "department_manager" ADD CONSTRAINT "fk_department_manager_emp_num" FOREIGN KEY("emp_num")
+REFERENCES "employees" ("emp_num");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_num" FOREIGN KEY("emp_num")
+REFERENCES "dept_emp" ("emp_num");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_num" FOREIGN KEY("emp_num")
+REFERENCES "dept_emp" ("emp_num");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_num" FOREIGN KEY("emp_num")
+REFERENCES "dept_emp" ("emp_num");
+
